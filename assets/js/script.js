c3a96e01f085e966ad8bc123cd6e40fd7de78908
Blehjo <blehjooo@gmail.com>
2021-10-18 11:26:03 -0500

    Updating pseudo code placement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Assignment Code
+//When the button is clicked
+var generateBtn = document.querySelector("#generate");
+
 //Creating variables that contain the tools to create a strong password
 var charLow = "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s-t-u-v-w-x-y-z";
 var charUp = "A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-D-T-U-V-W-X-Y-Z"
@@ -10,9 +14,12 @@ var upperArr = charUp.split("-");
 var numArr = charNum.split("-");
 var specArr = charSpec.split("-");
 
-//Character bank that will hold the necessary tools when prompted
-var charBank = "";
+var bank = "";
+var charBank = bank.split("");
+console.log(charBank);
 
+//Function is putting together an array that will be used to decide what our password will be made of.  
+//It also guides the user along, so no errors will be made.
 function userInput() {
   var passLength = prompt("How many characters do you want your password to be?");
   if (passLength < 8) {
@@ -23,21 +30,29 @@ function userInput() {
     return null;
   }
   var passCharLow = confirm("Do you want lowercase to be included");
-  if (passCharLow === true); {
-    charBank.concat(lowerArr);
+  if (passCharLow === true) {
+    var charBank = [].concat(lowerArr);
+    console.log(charBank);
   }
   var passCharUp = confirm("Do you want uppercase to be included");
-  if (passCharUp === true); {
-    charBank.concat(upperArr);
+  if (passCharUp === true) {
+    var charBank = charBank.concat(upperArr);
+    console.log(charBank);
   }
   var passCharNum = confirm("Do you want numbers to be included");
-  if (passCharNum === true); {
-    charBank.concat(numArr);
+  if (passCharNum === true) {
+    var charBank = charBank.concat(numArr);
+    console.log(charBank);
   }
   var passCharSpec = confirm("Do you want special characters to be included");
-  if (passCharSpec === true); {
-    charBank.concat(specArr);
-  }
+  if (passCharSpec === true) {
+    var charBank = charBank.concat(specArr);
+    console.log(charBank);
+  //Creates an alert if not a single character type is chosen.
+  } if (passCharLow === false && passCharUp === false && passCharNum === false && passCharSpec === false) {
+    alert("You have to choose at least one of the options to continue.");
+    return null;
+  } 
   //We want to create an object that contains our variables to return the object, so we expect it when we call user input.
   var passBank = {
     passLength: passLength,
@@ -46,38 +61,23 @@ function userInput() {
     passCharNum: passCharNum,
     passCharSpec: passCharSpec,
   }; 
-  return passBank;
+  return charBank;
 }
+// function Random(array) {
+//   var randomI = Math.floor(Math.random() * passLength.length);
+//   var randomEl = array[randomI];
 
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
+//   return randomEl;
+// }
+
+// Random(charBank);
+
+
+//Returns charBank array.  Contains the available options for a password based on the choices of the user.
 function generatePassword() {
   var passInput = userInput(); 
   console.log(passInput);
 }
-
-
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-
-
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-// Assignment Code
-//When the button is clicked
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -85,7 +85,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
